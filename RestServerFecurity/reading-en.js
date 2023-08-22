54d673bb04e8ce2e3f3132fb6a5053e7eb224a5a
Ricardo Reyes <rreyes@tribalworldwide.gt>
2023-08-22 14:05:49 -0500

    implementacion

---
+++ RestServerFecurity/reading-en.js
@@ -0,0 +1,23 @@
+//this file is created in order to handle errors returned by the server through code
+//and that can then be translated into different languages
+
+{
+    "00001"="mail is required to enter"
+    "00002"="password is required to enter"
+    "00003"="User / Password are not correct - mail"
+    "00004"="User / Password are not correct - password"
+    "00005"="User / Password are not correct - status: false"
+    "00006"="Talk to the administrator"
+    "00007"="The role is not registered in the DB"
+    "00008"="The mail is already registered"
+    "00009"="The id does not exist"
+    "00010"="Failed to generate token"
+    "00011"="name is required"
+    "00012"="The password must be more than 6 letters"
+    "00013"="The email is invalid"
+    "00014"="the date is required"
+    "00015"="status is required"
+    "00016"="There is no token in the request"
+    "00017"="Invalid token - user does not exist DB"
+    "00018"="Invalid token - stateful user: false"
+}
