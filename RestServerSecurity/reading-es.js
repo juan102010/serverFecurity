a53d6da8e9c360debd6e1f406a2bcd11b1a6baf8
Ricardo Reyes <rreyes@tribalworldwide.gt>
2023-09-08 10:10:00 -0500

    complementatacion

---
+++ RestServerSecurity/reading-es.js
@@ -0,0 +1,37 @@
+//este archibo esta creado con el fin de manejar los errores entregados por el server por medio de codigo
+//y que luego puedan ser traducidos en direfentes idiomas
+
+{
+ "00001"="El correo es obligatorio para ingresar"
+ "00002"="La contraseña es obligatoria para ingresar"
+ "00003"="Usuario / Password no son correctos - correo"
+ "00004"="Usuario / Password no son correctos - password"
+ "00005"="Usuario / Password no son correctos - estado: false"
+ "00006"="Hable con el administrador"
+ "00007"="El rol no está registrado en la BD"
+ "00008"="El correo ya está registrado"
+ "00009"="El id no existe "
+ "00010"="No se pudo generar el token"
+ "00011"="El nombre es obligatorio"
+ "00012"="El password debe de ser más de 6 letras"
+ "00013"="El correo no es válido"
+ "00014"="la fecha es obligatorio"
+ "00015"="el estado es obligatorio"
+ "00016"="No hay token en la petición"
+ "00017"="Token no válido - usuario no existe DB"
+ "00018"="Token no válido - usuario con estado: false"
+ "00019"="Token no válido para revisar los usuarios"
+ "00020"="El Ide_Usuario que se está intentando modificar no está registrado en la base de datos"
+ "00021"="El correo es obligatorio"
+ "00022"="el Id de la empresa es requerido"
+ "00023"="Este campo es requerido"
+ "00024"="No se encuentra el Ide_Rol"
+ "00025"="No se encuentra el Ide_Usuario"
+ "00026"="No se peude crear ya que el Ide_Usuario ya esta registrado en la base de datos y tiene que ser unico"
+ "00027"="No se encuentra el Ere_Id"
+ "00028"="No se encuentra el Ide_Aplicacion"
+ "00029"="No se encuentra el Emp_Id"
+ "00030"="No se encuentra el Ide_Modulo"
+ "00031"="No se encuentra el Ide_Funcionalidad"
+
+} 
